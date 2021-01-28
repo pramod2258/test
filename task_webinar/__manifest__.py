47f7cdcc5fdeb08d51926e6a169452f21d512856
pramod <sk@odoo.ie>
2021-01-28 19:55:51 +0530

    first commit

---
+++ task_webinar/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Task Webinar",
+    'version': '14.0.1.0.0',
+    'summary': """""",
+    'description': """""",
+    'author': "Pramo",
+
+    'website': "",
+    'depends': ['website','crm','base'],
+
+    'data': ['views/webinar_template.xml','views/website_menu_inherit.xml','data/website_asset.xml'
+
+    ],
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+}
